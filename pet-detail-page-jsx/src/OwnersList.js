d5ffc21258415597df8af34e097086b6d86bbc7d
Jerel <65048179+jerels@users.noreply.github.com>
2020-08-18 15:11:19 -0500

    OwnerLink setup/project finished

--- pet-detail-page-jsx/src/OwnersList.js
+++ pet-detail-page-jsx/src/OwnersList.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import OwnerLink from './OwnerLink';
 
 const OwnersList = props =>
     <>
         <h2>Owners</h2>
         <ul>
             {props.owners.map(owner =>
-                <li key={owner.id}>{owner.firstName}</li>
+                <li key={owner.id}>
+                    <OwnerLink href={owner.href} firstName={owner.firstName} lastName={owner.lastName} />
+                </li>
             )}
         </ul>
     </>
