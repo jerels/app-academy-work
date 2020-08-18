8fc8c8327c16f24001a4f25030ff270fd616b769
Jerel <65048179+jerels@users.noreply.github.com>
2020-08-18 13:13:57 -0500

    PetDetails setup

--- pet-detail-page-jsx/src/PetDetailPage.js
+++ pet-detail-page-jsx/src/PetDetailPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Navigation from './Navigation';
+import PetDetails from './PetDetails';
 
-const PetDetailPage = () =>
-    <Navigation />
+const PetDetailPage = props =>
+    <React.Fragment>
+        <Navigation />
+        <PetDetails pet={props.pet} />
+    </React.Fragment>
     ;
 
 export default PetDetailPage;
---
+++ pet-detail-page-jsx/src/PetDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PetDetails = props =>
+    <div></div>
+    ;
+
+export default PetDetails;
