afcb0c5d43bff64dec57c064f72175a4ad95696f
Jerel <65048179+jerels@users.noreply.github.com>
2020-08-19 17:38:29 -0500

    finished intro

---
+++ homework/react-router/src/Users.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Users = () => {
+    return (
+        <h1>This is the users' index!</h1>
+    )
+}
+
+export default Users;
--- homework/react-router/src/index.js
+++ homework/react-router/src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Users from './Users';
+
+
+const Root = () => {
+  const users = {
+    1: { name: 'Andrew' },
+    2: { name: 'Raymond' }
+  };
+
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>Hi, I'm Root!</h1>
+        <Route exact path='/' component={App} />
+        <Route exact path='/users' render={() => <Users users={users} />} />
+        <Route path='/hello' render={() => <h1>Hello!!</h1>} />
+        <Route path='/users/:userId' render={(props) => <Profile users={users} {...props} />} />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
