441b8d350d0d04be400ed5883643916ac8a32d38
Jerel <65048179+jerels@users.noreply.github.com>
2020-08-21 18:21:30 -0500

    project setup/end of phase one

---
+++ projects/context-to-do-list/src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Task from './Task';
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <ul>
+
+            </ul>
+        );
+    }
+}
+
+export default TodoList;
