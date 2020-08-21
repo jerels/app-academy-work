ccd1ad333c4204473fb54972bcb0256850e3f92c
Jerel <65048179+jerels@users.noreply.github.com>
2020-08-21 17:44:15 -0500

    express server

---
+++ projects/react-build-project/express-server/public/precache-manifest.adf818b35c94363a0acd92df9165aa30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cbc6cd58a6538e6c7ce103b76d4ed0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "763db0a05c3224843b96",
+    "url": "/static/css/main.c73cb05f.chunk.css"
+  },
+  {
+    "revision": "b22c9ba60b0d609baba0",
+    "url": "/static/js/2.cf67a1cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cf67a1cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "763db0a05c3224843b96",
+    "url": "/static/js/main.07f58000.chunk.js"
+  },
+  {
+    "revision": "256780cce990331d9974",
+    "url": "/static/js/runtime-main.83a27b56.js"
+  },
+  {
+    "revision": "b39b36ce621881e05ec3ddb7ed608238",
+    "url": "/static/media/mushLadies.b39b36ce.jpg"
+  }
+]);
