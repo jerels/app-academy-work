de5221fd72db4fce1349b017a081c377834d4918
Jerel <65048179+jerels@users.noreply.github.com>
2020-08-21 16:53:16 -0500

    images done

--- projects/exploring-react-builds/src/App.js
+++ projects/exploring-react-builds/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeadingA from './css-modules/HeadingA';
 import HeadingB from './css-modules/HeadingB';
+import Image from './image/Image';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>Exploring React Builds!</h1>
       <HeadingA />
       <HeadingB />
+      <Image />
     </div>
   );
 }
