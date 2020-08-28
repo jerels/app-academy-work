826098b87809e297c94d36a4846d3d347f7410c3
Jerel <65048179+jerels@users.noreply.github.com>
2020-08-28 14:33:08 -0500

    pokebrowser finished

--- projects/react-hooks-pokedex/react-hooks-pokedex-starter-master/src/PokemonBrowser.js
+++ projects/react-hooks-pokedex/react-hooks-pokedex-starter-master/src/PokemonBrowser.js
@@ -1,36 +1,64 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink, Route } from 'react-router-dom';
 import PokemonDetail from './PokemonDetail';
 import LogoutButton from './LogoutButton';
 
-class PokemonBrowser extends Component {
-  render() {
-    const { pokemon } = this.props;
-    if (!pokemon) {
-      return null;
-    }
-    return (
-      <main>
-        <LogoutButton />
-        <nav>
-          {pokemon.map((pokemon) => {
-            return (
-              <NavLink key={pokemon.name} to={`/pokemon/${pokemon.id}`}>
-                <div className="nav-entry">
-                  <div
-                    className="nav-entry-image"
-                    style={{ backgroundImage: `url('${pokemon.imageUrl}')` }}
-                  ></div>
-                  <h1>{pokemon.name}</h1>
-                </div>
-              </NavLink>
-            );
-          })}
-        </nav>
-        <Route path="/pokemon/:id" component={PokemonDetail} />
-      </main>
-    );
-  }
+const PokemonBrowser = props => {
+  const { pokemon } = props;
+
+  if (!pokemon) return null;
+
+  return (
+    <main>
+      <LogoutButton />
+      <nav>
+        {pokemon.map((pokemon) => {
+          return (
+            <NavLink key={pokemon.name} to={`/pokemon/${pokemon.id}`}>
+              <div className="nav-entry">
+                <div
+                  className="nav-entry-image"
+                  style={{ backgroundImage: `url('${pokemon.imageUrl}')` }}
+                ></div>
+                <h1>{pokemon.name}</h1>
+              </div>
+            </NavLink>
+          );
+        })}
+      </nav>
+      <Route path="/pokemon/:id" component={PokemonDetail} />
+    </main>
+  );
 }
+
+// class PokemonBrowser extends Component {
+//   render() {
+//     const { pokemon } = this.props;
+//     if (!pokemon) {
+//       return null;
+//     }
+//     return (
+//       <main>
+//         <LogoutButton />
+//         <nav>
+//           {pokemon.map((pokemon) => {
+//             return (
+//               <NavLink key={pokemon.name} to={`/pokemon/${pokemon.id}`}>
+//                 <div className="nav-entry">
+//                   <div
+//                     className="nav-entry-image"
+//                     style={{ backgroundImage: `url('${pokemon.imageUrl}')` }}
+//                   ></div>
+//                   <h1>{pokemon.name}</h1>
+//                 </div>
+//               </NavLink>
+//             );
+//           })}
+//         </nav>
+//         <Route path="/pokemon/:id" component={PokemonDetail} />
+//       </main>
+//     );
+//   }
+// }
 
 export default PokemonBrowser;
