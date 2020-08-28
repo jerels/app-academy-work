b5d3b30d13ad66774dafe7e3ea05af3fc66759ca
Jerel <65048179+jerels@users.noreply.github.com>
2020-08-28 15:00:02 -0500

    loginpanel finished

--- projects/react-hooks-pokedex/react-hooks-pokedex-starter-master/src/LoginPanel.js
+++ projects/react-hooks-pokedex/react-hooks-pokedex-starter-master/src/LoginPanel.js
@@ -1,59 +1,110 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
+const LoginPanel = props => {
+  const [email, setEmail] = useState('demo@example.com');
+  const [password, setPassword] = useState('password');
+  const [currentUserId, setUserId] = useState(null);
 
-class LoginPanel extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: 'demo@example.com',
-      password: 'password',
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.updateEmail = this.updateValue('email');
-    this.updatePassword = this.updateValue('password');
-  }
-
-  async handleSubmit(e) {
+  const handleSubmit = async e => {
     e.preventDefault();
     const response = await fetch(`/api/session`, {
       method: 'put',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(this.state),
+      body: JSON.stringify({ email, password }),
     });
 
     if (response.ok) {
       const { player } = await response.json();
-      this.props.updateUser(player.id);
-      this.setState({ currentUserId: player.id });
+      props.updateUser(player.id);
+      setUserId({ currentUserId: player.id });
     }
   }
 
-  updateValue = name => e => {
-    this.setState({ [name]: e.target.value });
+  const updateValue = e => {
+    const type = e.target.type;
+    const value = e.target.value;
+
+    if (type === 'email') {
+      setEmail(value);
+    } else if (type === 'password') {
+      setPassword(value);
+    }
   }
 
-  render() {
-    const { email, password, currentUserId } = this.state;
-    if (currentUserId) {
-      return <Redirect to="/" />;
-    }
-    return (
-      <main className="centered middled">
-        <form onSubmit={this.handleSubmit}>
-          <input type="text"
-                placeholder="Email"
-                value={email}
-                onChange={this.updateEmail} />
-          <input type="password"
-                placeholder="Password"
-                value={password}
-                onChange={this.updatePassword} />
-          <button type="submit">Login</button>
-        </form>
-      </main>
-    );
+  if (currentUserId) {
+    return <Redirect to="/" />;
   }
+
+  return (
+    <main className="centered middled">
+      <form onSubmit={handleSubmit}>
+        <input type="email"
+          placeholder="Email"
+          value={email}
+          onChange={updateValue} />
+        <input type="password"
+          placeholder="Password"
+          value={password}
+          onChange={updateValue} />
+        <button type="submit">Login</button>
+      </form>
+    </main>
+  );
 }
+
+// class LoginPanel extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       email: 'demo@example.com',
+//       password: 'password',
+//     };
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.updateEmail = this.updateValue('email');
+//     this.updatePassword = this.updateValue('password');
+//   }
+
+//   async handleSubmit(e) {
+//     e.preventDefault();
+//     const response = await fetch(`/api/session`, {
+//       method: 'put',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify(this.state),
+//     });
+
+//     if (response.ok) {
+//       const { player } = await response.json();
+//       this.props.updateUser(player.id);
+//       this.setState({ currentUserId: player.id });
+//     }
+//   }
+
+//   updateValue = name => e => {
+//     this.setState({ [name]: e.target.value });
+//   }
+
+//   render() {
+//     const { email, password, currentUserId } = this.state;
+//     if (currentUserId) {
+//       return <Redirect to="/" />;
+//     }
+//     return (
+//       <main className="centered middled">
+//         <form onSubmit={this.handleSubmit}>
+//           <input type="email"
+//             placeholder="Email"
+//             value={email}
+//             onChange={this.updateEmail} />
+//           <input type="password"
+//             placeholder="Password"
+//             value={password}
+//             onChange={this.updatePassword} />
+//           <button type="submit">Login</button>
+//         </form>
+//       </main>
+//     );
+//   }
+// }
 
 export default LoginPanel;
