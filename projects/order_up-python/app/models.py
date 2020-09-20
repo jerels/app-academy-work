036f9693ce4ce652abb84c98c6dfd02c9059e45c
Jerel <65048179+jerels@users.noreply.github.com>
2020-09-19 20:30:31 -0500

    added menu models

--- projects/order_up-python/app/models.py
+++ projects/order_up-python/app/models.py
@@ -23,3 +23,34 @@ class Employee(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+class Menu(db.Model):
+    __tablename__ = 'menus'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+
+    items = relationship('MenuItem', back_populates='menus')
+
+
+class MenuItem(db.Model):
+    __tablename__ = 'menu_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    menu_id = db.Column(db.Integer, nullable=False, ForeignKey('menu.id'))
+    menu_type_id = db.Column(db.Integer, nullable=False, ForeignKey('menu_type.id'))
+
+    menu = relationship('Menu', back_populates='menu_items')
+    type = relationship('MenuItemType', back_populates='menu_items')
+
+
+class MenuItemType(db.Model):
+    __tablename__ = 'menu_item_types'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+
+    items = relationship('MenuItem', back_populates='menu_item_types')
